d22835c8b3558608bde4d93502a575a279afc3db
vitoria.sousa <maria.v.cosmo@gmail.com>
2025-02-18 11:57:17 -0300

    feat: criando o header da tela de repositórios

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Container } from "./styles";
+import { Container, Owner, Loading, BackButton } from "./styles";
+import { FaArrowLeft } from "react-icons/fa";
 import api from "../../services/api";
 
 export default function Repositorio() {
-  const { repos } = useParams();
+  const { repositorio } = useParams();
   // return <h1 style={{ color: "#fff" }}>{repositorio}</h1>;
 
-  const [repositorio, setRepositorio] = useState({});
+  const [repositorios, setRepositorios] = useState({});
   const [issues, setIssues] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function load() {
-      const nomeRepo = repos;
+      const nomeRepo = repositorio;
+      console.log("nome", nomeRepo);
 
       // Vai fazer duas requisições ao mesmo tempo
       const [repositorioData, issuesData] = await Promise.all([
@@ -29,11 +31,33 @@ export default function Repositorio() {
       // console.log(repositorioData.data);
       // console.log(issuesData.data);
 
-      setRepositorio(repositorioData.data);
+      setRepositorios(repositorioData.data);
       setIssues(issuesData.data);
       setLoading(false);
     }
     load();
-  }, [repos]);
-  return <Container></Container>;
+  }, [repositorio]);
+
+  if (loading) {
+    return (
+      <Loading>
+        <h1>Carregando...</h1>
+      </Loading>
+    );
+  }
+  return (
+    <Container>
+      <BackButton to="/">
+        <FaArrowLeft color="#000" size={30} />
+      </BackButton>
+      <Owner>
+        <img
+          src={repositorios.owner.avatar_url}
+          alt={repositorios.owner.login}
+        />
+        <h1>{repositorios.name}</h1>
+        <p>{repositorios.description}</p>
+      </Owner>
+    </Container>
+  );
 }
